615cb713e6941d30290d3c7e3d785513c82b8e2a
Noor Awied <nooorawied@gmail.com>
2022-04-23 19:31:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "john",
+  streetAdress: "mulburry",
+};
+
+const updateEmployeeWithKeyAndValue = (object, newkey, value) => {
+  return {
+    ...object,
+    [newkey]: value,
+  };
+};
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (object, newkey, value) => {
+  object[newkey] = value;
+  return object;
+};
+
+const deleteFromEmployeeByKey = (object, newkey) => {
+  const newObj = { ...object };
+  delete newObj[newkey];
+  return newObj;
+};
+
+const destructivelyDeleteFromEmployeeByKey = (object, newkey) => {
+  delete object[newkey];
+  return object;
+};
+
+console.log(updateEmployeeWithKeyAndValue(employee, "height", 170));
+console.log(
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "height", 170)
+);
+console.log(deleteFromEmployeeByKey(employee, "height"));
+console.log(employee);
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "height"));
+console.log(employee);
